397db1df9107e7e43a10498a54311aa607c2fefe
ja-ilija <subasic.ilija@gmail.com>
2022-03-01 15:33:30 -0300

    add transport service and balena block

--- log_transporter/src/balena/labs/fleet_analytics/transporter/api/fa_api.py
+++ log_transporter/src/balena/labs/fleet_analytics/transporter/api/fa_api.py
@@ -1,34 +1,51 @@
 import logging
 import os
 import sys
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, redirect, url_for
 import yaml
 #from balena.labs.fleet_analytics.transporter.kafka_rest.kafka_rest_transporter import KafkaTopicTransporter
 
 app = Flask(__name__)
 
 @app.route("/kafka-rest", methods=['POST'])
-def sendToKafkaProxy():
+def sendToKafkaRest():
+    """
+
+    :return:
+    """
+    message = request.get_json(force=True, silent=True, cache=False)
+    logger.info("sending to kafka rest %s".format(message))
+    return send_kafka_rest(message)
+
+
+def send_kafka_rest(message):
     """
     Sends a message to kafka proxy transformer
     :return:
     """
-    message = request.get_json()
-    print(message)
-    logger.info("sending to kafka rest %s".format(message))
-    response = kafka_rest_transporter.send_message (message)
+    response = kafka_rest_transporter.send_message(message)
     return jsonify(response)
+
+
+@app.route("/", methods=['POST'])
+def processRequest():
+    """
+    redirects to specified endpoint
+    :return:
+    """
+    message = request.get_json(force=True, silent=True, cache=False)
+    if sink_type == "kafka_rest":
+        return send_kafka_rest(message)
 
 if __name__ == '__main__':
 
     logger = logging.getLogger(__name__)
-
-    with open(sys.argv[1], "r") as fh:
-        # probably needs some sort of smarter paameter parsing to be able to use some kind of reflection
-        params = yaml.safe_load(fh)
-        kafka_rest = params.get('kafka_rest', None)
-        if kafka_rest:
-            kafka_rest_url = kafka_rest['proxy_url']
-            import balena.labs.fleet_analytics.transporter.kafka_rest.kafka_rest_transporter as k
-            kafka_rest_transporter = k.KafkaTopicTransporter(kafka_rest_url)
-        app.run(host='0.0.0.0', debug=True, port=5000)
+    # this probably needs to be somehow abstracted so that it can take any value
+    # POST requests cannot be reddirected 
+    sink_type = os.getenv('BALENA_FLEET_SINK_TYPE', '')
+    kafka_rest_url = os.getenv('BALENA_FLEET_ANALYTICS_KAFKA_URL', '')
+    transporter_port = os.getenv('BALENA_FLEET_ANALYTICS_PORT', 5000)
+    if kafka_rest_url:
+        import balena.labs.fleet_analytics.transporter.kafka_rest.kafka_rest_transporter as k
+        kafka_rest_transporter = k.KafkaTopicTransporter(kafka_rest_url)
+    app.run(host='0.0.0.0', debug=True, port=transporter_port)
